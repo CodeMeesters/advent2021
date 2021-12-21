a0fc79f89362e34be44c1494dba2226d8dfbe6cb
Paul Eigeman <paul.eigeman@tno.nl>
2021-12-21 14:41:10 +0100

    Preps for day 2

---
+++ day2/day2/src/main/java/advent2021/day2/App.java
@@ -0,0 +1,13 @@
+package advent2021.day2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
